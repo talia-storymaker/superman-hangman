0274cdc9f9b31f33ddb2c8259897cd7b0496aa7e
Talia DeGisi <talia.j.degisi@gmail.com>
2020-07-08 21:58:28 -0500

    Fix issues related to listening for keys
    Make it so that keys are only listened to while the game is running and
    that pressing a key multiple times in a game doesn't do anything

--- js/scripts.js
+++ js/scripts.js
@@ -79,26 +79,29 @@ const app = new Vue({
     },
     methods: {
         guessLetter: function(letter) {
-            let caseInsensitiveLetter = new RegExp(letter, 'i');
-            let lettersCorrectlyGuessed = 0;
-            for (let i = 0; i < this.chosenWord.length; i++) {
-                if (caseInsensitiveLetter.test(this.chosenWord[i])) {
-                    this.wordWithBlanks[i] = this.chosenWord[i];
-                    lettersCorrectlyGuessed++;
+            if (this.gameRunning) {
+                let caseInsensitiveLetter = new RegExp(letter, 'i');
+                let lettersCorrectlyGuessed = 0;
+                for (let i = 0; i < this.chosenWord.length; i++) {
+                    if (caseInsensitiveLetter.test(this.chosenWord[i])) {
+                        this.wordWithBlanks[i] = this.chosenWord[i];
+                        lettersCorrectlyGuessed++;
+                    }
                 }
-            }
-            if (lettersCorrectlyGuessed === 0) {
-                this.incorrectGuesses++;
-            }
-            document.getElementById(letter).disabled = 'disabled';
-            this.wordWithBlanksCurrent = this.wordWithBlanks.join('');
-            if (!this.wordWithBlanks.includes('_')) {
-                this.winStatus = 'You won!';
-                this.gameRunning = false;
-            }
-            if (this.incorrectGuesses === 6) {
-                this.winStatus = 'You lose!';
-                this.gameRunning = false;
+                if (lettersCorrectlyGuessed === 0) {
+                    this.incorrectGuesses++;
+                }
+                document.getElementById(letter).disabled = 'disabled';
+                this.wordWithBlanksCurrent = this.wordWithBlanks.join('');
+                if (!this.wordWithBlanks.includes('_')) {
+                    this.winStatus = 'You won!';
+                    this.gameRunning = false;
+                }
+                if (this.incorrectGuesses === 6) {
+                    this.winStatus = 'You lose!';
+                    this.gameRunning = false;
+                }
+                console.log(letter);
             }
         },
         newGame: function() {
@@ -112,6 +115,8 @@ const app = new Vue({
 
 document.addEventListener('keyup', function(event) {
     if (event.key === 'a' || event.key === 'b' || event.key === 'c' || event.key === 'd' || event.key === 'e' || event.key === 'f' || event.key === 'g' || event.key === 'h' || event.key === 'i' || event.key === 'j' || event.key === 'k' || event.key === 'l' || event.key === 'm' || event.key === 'n' || event.key === 'o' || event.key === 'p' || event.key === 'q' || event.key === 'r' || event.key === 's' || event.key === 't' || event.key === 'u' || event.key === 'v' || event.key === 'w' || event.key === 'x' || event.key === 'y' || event.key === 'z') {
-        app.guessLetter(event.key);
+        if (!document.getElementById(event.key).disabled) {
+            app.guessLetter(event.key);
+        }
     }
 });
